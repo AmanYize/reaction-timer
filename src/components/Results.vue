508686f546ebc3928e2a0e1dc78d266183517ef9
AmanYize <amanexo19@gmail.com>
2025-02-21 20:53:43 +0300

    'finished

---
+++ src/components/Results.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="Results">
+    <h1>Results</h1>
+    <p class="score">Reaction time: {{ score }} ms</p>
+    <p class="rank">Reaction time: {{ rank }} ms</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Ninja Fingers";
+    } else if (this.score < 400) {
+      this.rank = "Rapid Reflexes";
+    } else {
+      this.rank = "Snail Pace...";
+    }
+  },
+};
+</script>
+
+<style>
+.Results {
+  width: 400px;
+  border-radius: 20px;
+  background: #0faf87;
+  text-align: center;
+  color: white;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+.score {
+  font-size: 1.2em;
+}
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  background: white;
+  border-radius: 3px;
+  padding: 5px 10px;
+}
+</style>
